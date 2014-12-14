4c68ef0a25bf63653813c3945b5ebff8cc729175
ashm87 <amorris8@uclan.ac.uk>
2014-12-14 03:31:09 +0000

    Version 0.2

---
+++ add-item-submission.php
@@ -0,0 +1,54 @@
+<?php
+    /**
+     * Created by PhpStorm.
+     * User: Ashley Morris
+     * Date: 04/12/2014
+     * Time: 11:52
+     */
+    //Adds a single item to the menu database. This will be only on the restaurant side of things so no android required.
+    require("config.inc.php");
+
+    if (isset($_POST['submit'])) {
+        //the form was submitted by the user
+
+        //The initial insert query without the parameters, protection from SQL injection
+        $query = "INSERT INTO menu (name, price, description, category, stock)
+                        VALUES (:name, :price, :description, :category, :stock)";
+
+        /*Query_params is an array that contains all the parameters for the SQL statement.
+         it gets the parameters from the HTML post request that is sent with the webform.*/
+        $query_params[':name'] = $_POST['item_name'];
+        $query_params[':price'] = $_POST['item_price'];
+        $query_params[':description'] = $_POST['item_description'];
+        $query_params[':category'] = $_POST['item_category'];
+        $query_params[':stock'] = $_POST['item_stock'];
+
+        //Validate information here and redirect to form if necessary
+
+        try {
+            //try to execute the query.
+
+            //prepare the statement without the parameters.
+            $stmt = $db->prepare($query);
+
+            //execute the query with the parameters
+            $result = $stmt->execute($query_params);
+        }
+        catch (PDOException $ex) {
+            //Error variable to be passed back via URL and read at the other end.
+            $error = "Database error, could not submit";
+            header("Location: scripts/add-item.php?e=" . urldecode($error));
+            exit;
+        }
+
+        $success = "{$query_params[':name']} has been added to the menu successfully";
+        header("Location: scripts/add-item.php?s=" . urldecode($success));
+        exit;
+    }
+
+    else {
+        //If navigated to this page with out pushing the submit button then redirect
+        header("Location: scripts/add-item.php");
+        exit;
+    }
+?>
---
+++ scripts/add-item.php
@@ -0,0 +1,149 @@
+<!DOCTYPE html>
+<html>
+<head lang="en">
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Add Item</title>
+
+    <link href="../css/simple-sidebar.css" rel="stylesheet">
+    <link rel="stylesheet" href="../css/bootstrap.min.css">
+    <link href="jumbotron.css" rel="stylesheet">
+    <link rel="stylesheet" href="../css/custom.css">
+</head>
+
+<body>
+<!-- navigation bar set up -->
+<nav class="navbar navbar-inverse navbar-fixed-top" role="navigation">
+    <div class="container-fluid ">
+        <div class="navbar-header">
+            <a href="#" class="navbar-brand">Go Eat</a>
+
+            <div id="buttons">
+                <a href="#menu-toggle" role="button" class="btn btn-default navbar-btn" id="menu-toggle">
+                    <span class="glyphicon glyphicon-align-justify"></span>
+                </a>
+                <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#navbar">
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                </button>
+            </div>
+        </div>
+        <div id="navbar" class="collapse navbar-collapse">
+            <ul class="nav navbar-nav navbar-right">
+                <li><a href="../index.htm">Home</a></li>
+                <li class="active"><a href="../dashboard.html">Dashboard</a></li>
+                <li><a href="#">About</a></li>
+                <li><a href="#">Contact</a></li>
+            </ul>
+        </div>
+    </div>
+</nav>
+<!-- /#wrapper -->
+<div id="wrapper">
+    <!-- Sidebar -->
+    <div id="sidebar-wrapper">
+        <ul class="sidebar-nav">
+            <li class="sidebar-brand">
+                <a href="#">Start Bootstrap</a>
+            </li>
+            <li><a href="#">Overview</a></li>
+            <li class="active"><a href="add-item.php">Add Menu Item</a></li>
+        </ul>
+    </div>
+
+    <!-- /#sidebar-wrapper -->
+
+    <!-- Page Content -->
+    <div id="page-content-wrapper" style="padding: 60px">
+        <div class="container-fluid">
+            <div class="row">
+                <div class="col-lg-12">
+                    <h1>Add Menu Item</h1>
+
+                    <?php
+                        //Checks http 'GET' to see if variables 'e' or 's' have been set.
+                        if (isset($_GET['s'])) {
+                            ?>
+                            <!--Add a positive alert that is dismissable-->
+                            <div class="alert alert-success alert-dismissable" role="alert">
+                                <button type="button" class="close" data-dismiss="alert"><span
+                                        aria-hidden="true">&times;</span>
+                                    <span class="sr-only">Close</span></button>
+                                <?php echo $_GET['s']; ?>
+                            </div>
+                        <?php
+                        } else {
+                            if (isset($_GET['e'])) {
+                                ?>
+                                <div class="alert alert-danger" role="alert">
+                                    <strong>Error: </strong><?php echo $_GET['e']; ?>
+                                </div>
+                            <?php
+                            }
+                        }
+                    ?>
+
+                    <br>
+
+                    <form class="form-horizontal" method="POST" role="form" action="../add-item-submission.php">
+                        <div class="form-group">
+                            <label for="item_name" class="col-sm-2 control-label">Item Name:</label>
+
+                            <div class="col-md-4">
+                                <input type="text" class="form-control" name="item_name" placeholder="Item Name">
+                            </div>
+                        </div>
+                        <div class="form-group">
+                            <label for="item_price" class="col-sm-2 control-label">Item Price:</label>
+
+                            <div class="col-md-4">
+                                <input type="text" class="form-control" name="item_price" placeholder="Item Price">
+                            </div>
+                        </div>
+                        <div class="form-group">
+                            <label for="item_description" class="col-sm-2 control-label">Description:</label>
+
+                            <div class="col-md-4">
+                                <textarea class="form-control" rows="5" name="item_description" placeholder="Description">
+                                    </textarea>
+                            </div>
+                        </div>
+                        <div class="form-group">
+                            <label for="item_category" class="col-sm-2 control-label">Category:</label>
+
+                            <div class="col-md-4">
+                                <input type="text" class="form-control" name="item_category" placeholder="Category">
+                            </div>
+                        </div>
+                        <div class="form-group">
+                            <label for="item_stock" class="col-sm-2 control-label">Stock:</label>
+
+                            <div class="col-md-4">
+                                <input type="text" class="form-control" name="item_stock" placeholder="Stock">
+                            </div>
+                        </div>
+                        <div class="form-group">
+                            <div class="col-sm-offset-2 col-sm-10">
+                                <button type="submit" name="submit" class="btn btn-primary">Add Item</button>
+                            </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+
+<!--Footer stuff, jquery import. Speeds page loading if at the bottom.-->
+<script src="http://code.jquery.com/jquery-1.11.0.min.js"></script>
+<script src="../js/bootstrap.js"></script>
+
+<!-- Menu Toggle Script -->
+<script>
+    $("#menu-toggle").click(function (e) {
+        e.preventDefault();
+        $("#wrapper").toggleClass("toggled");
+    });
+</script>
+</body>
